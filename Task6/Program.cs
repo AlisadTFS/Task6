3ea5b5956ed94fecd6d02fbac0d285bbbeca3849
Alex <virdata@yandex.ru>
2022-03-10 23:05:32 +0800

    Задание №6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -10,6 +10,24 @@ namespace Task6
     {
         static void Main(string[] args)
         {
+            const daysofweek workallday = (daysofweek)0b_1111111;
+            string nameone = "Автосалон";
+            string nametwo = "Магазин";
+            daysofweek workdayone = (daysofweek.Вториник | daysofweek.Восскресенье) ^ workallday;
+            daysofweek workdaytwo = (daysofweek.Суббота | daysofweek.Четверг) ^ workallday;
+            Console.WriteLine(nameone+" работает: " + workdayone + ".");
+            Console.WriteLine(nametwo + " работает: " + workdaytwo + ".");
+        }
+        [Flags]
+        enum daysofweek
+        {
+             Понедельник = 1 << 0,
+                Вториник = 1 << 1,
+                   Среда = 1 << 2,
+                 Четверг = 1 << 3,
+                 Пятница = 1 << 4,
+                 Суббота = 1 << 5,
+            Восскресенье = 1 << 6
         }
     }
 }
